3fae6bde6505cfc32c5d5a74ebdf7710bae8fffd
ImHotBtw <57193518+ImHotBtw@users.noreply.github.com>
2020-01-08 16:37:23 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -1313,8 +1313,8 @@ function getInfo() {
 	}
 	document.querySelector("#hud > div.stats > div:nth-child(3)")
 
-	Player.tag != localStorage.getItem("teamtag") ? (Player.tag = localStorage.getItem("teamtag")) : Player.tag = Player.tag;
-	var mass=document.querySelector("#hud > div.stats > div:nth-child(3)").innerText.indexOf(Player.tag);
+	tag != localStorage.getItem("teamtag") ? (tag = localStorage.getItem("teamtag")) : tag = tag;
+	var mass=document.querySelector("#hud > div.stats > div:nth-child(3)").innerText.indexOf(tag);
 	mass.id = "massCount"
 	console.log("incorrect username or password")
 	console.log(mass)
