cd8b51b80a69349c70b9910c384fa58973549193
ImHotBtw <57193518+ImHotBtw@users.noreply.github.com>
2020-01-22 19:13:01 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -192,7 +192,7 @@ setTimeout(()=>{
 	document.querySelector("#toggleHudV").setAttribute("onclick", "showHudV()");
 	}
 	
-	  document.querySelectorAll("#vanis-io_300x250")[0].innerHTML += VEX4;
+	  document.querySelectorAll("#vanis-io_300x250")[0].innerHTML += VEX;
 	  document.querySelector(".social-container").innerHTML += '<a id="toggleHudV" style="background:#b5b3b8;cursor:pointer;outline:none;border:0;padding:5px;color:#dadada;box-shadow:0 0 1px 1px #000;border-radius:4px;font-size:16px;text-shadow:1px 1px 2px #000;margin-left:10px;" onclick="showHudV()">Functions Beta</a>';
 	
 		window._$ = selector => {
