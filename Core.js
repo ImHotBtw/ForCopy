ef064f95e37c2f3a8c234c19f591b66a171e3607
ImHotBtw <57193518+ImHotBtw@users.noreply.github.com>
2020-02-01 10:20:01 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -1259,7 +1259,7 @@ function getInfo() {
 	for(var i = 0; i < objPeople.length; i++) {
 		if(username == objPeople[i].username && password == objPeople[i].password) {
 			
-			var team = document.getElementById("teamtag").value;
+			var team = document.getElementById("teamtag").innerText;
 			alert("StandOff - Welcome Back Admin!")
 			hideHud();
 			console.log(team)
